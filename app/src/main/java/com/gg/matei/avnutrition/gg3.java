bb3e819cbb6f7b0d7e69370b281b3528ce62a99b
Matei <olteanmat@gmail.com>
2018-01-16 08:58:26 +0200

    save my password and username

--- app/src/main/java/com/gg/matei/avnutrition/gg3.java
+++ app/src/main/java/com/gg/matei/avnutrition/gg3.java
@@ -1,13 +1,19 @@
 package com.gg.matei.avnutrition;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,7 +23,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class gg3 extends AppCompatActivity implements View.OnClickListener {
+public class gg3 extends AppCompatActivity implements View.OnClickListener, TextWatcher, CompoundButton.OnCheckedChangeListener {
 
     private Button login_button;
     private EditText editTextEmail;
@@ -26,6 +32,13 @@ public class gg3 extends AppCompatActivity implements View.OnClickListener {
 
     private ProgressDialog progressDialog;
     private FirebaseAuth firebaseAuth;
+    private CheckBox savepassword;
+    private static final String PREF_NAME="prefs";
+    private static final String KEY_REMEMBER = "remember";
+    private static final String KEY_USERNAME = "username";
+    private static final String KEY_PASS = "password";
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
 
     @Override
@@ -51,7 +64,61 @@ public class gg3 extends AppCompatActivity implements View.OnClickListener {
 
         login_button.setOnClickListener(this);
         textViewLogin.setOnClickListener(this);
+
+        sharedPreferences = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        savepassword = (CheckBox)findViewById(R.id.savepassword);
+
+        if(sharedPreferences.getBoolean(KEY_REMEMBER, false))
+            savepassword.setChecked(true);
+        else
+            savepassword.setChecked(false);
+
+        editTextEmail.setText(sharedPreferences.getString(KEY_USERNAME, ""));
+        editTextPassword.setText(sharedPreferences.getString(KEY_PASS, ""));
+
+        editTextEmail.addTextChangedListener(this);
+        editTextPassword.addTextChangedListener(this);
+        savepassword.setOnCheckedChangeListener(this);
+
     }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        managePrefs();
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        managePrefs();
+
+    }
+
+    private void managePrefs(){
+        if(savepassword.isChecked()){
+            editor.putString(KEY_USERNAME, editTextEmail.getText().toString().trim());
+            editor.putString(KEY_PASS, editTextPassword.getText().toString().trim());
+            editor.putBoolean(KEY_REMEMBER, true);
+            editor.apply();
+        }else{
+            editor.putBoolean(KEY_REMEMBER, false);
+            editor.remove(KEY_PASS);//editor.putString(KEY_PASS,"");
+            editor.remove(KEY_USERNAME);//editor.putString(KEY_USERNAME, "");
+            editor.apply();
+        }
+    }
+
 
     private void userLogin(){
 
